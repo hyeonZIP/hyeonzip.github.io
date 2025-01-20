dff6e346d9291e4183dbe82c92e4831f848c155d
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-01-20 22:49:43 +0900

    feat: 다크모드 구현

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
-
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
@@ -12,6 +5,28 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
+  const [theme, setTheme] = React.useState('light');
+
+  React.useEffect(()=> {
+    if(typeof window !== 'undefined') {
+      const isUserColorTheme = localStorage.getItem('color-theme');
+      const isOsColorTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+      const getUserTheme = isUserColorTheme ? isUserColorTheme : isOsColorTheme;
+      //로컬에 저장된 선호 테마가 없으면 현재 os에 적용된 테마를 사용 
+
+      setTheme(getUserTheme);
+      document.documentElement.setAttribute('color-theme', getUserTheme);
+      localStorage.setItem('color-theme', getUserTheme);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    document.documentElement.setAttribute('color-theme', newTheme);
+    localStorage.setItem('color-theme', newTheme);
+  };
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -24,7 +39,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} theme={theme} toggleTheme={toggleTheme} />
       <div
         style={{
           margin: `0 auto`,
