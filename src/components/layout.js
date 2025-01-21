a3ae223eed2a8d8f76130e97718c40431210f058
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-01-21 09:53:26 +0900

    feat: 변수명 변경 및 * import 제거

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, {useState,useEffect,useCallback} from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
@@ -6,27 +6,27 @@ import "./layout.css"
 
 const Layout = ({ children }) => {
   // 초기 상태를 'light'로 설정
-  const [theme, setTheme] = React.useState('light');
+  const [theme, setTheme] = useState('light');
 
   // 클라이언트에서만 실행되는 로직
-  React.useEffect(() => {
+  useEffect(() => {
     if (typeof window !== 'undefined') {
       // 로컬 스토리지에서 사용자 테마 가져오기
-      const isUserColorTheme = localStorage.getItem('color-theme');
+      const localStorageTheme = localStorage.getItem('color-theme');
       // OS 기본 테마 확인
-      const isOsColorTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+      const osTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
       // 최종 테마 결정
-      const initialTheme = isUserColorTheme || isOsColorTheme;
+      const currentTheme = localStorageTheme || osTheme;
 
       // 상태와 DOM에 테마 적용
-      setTheme(initialTheme);
-      document.documentElement.setAttribute('color-theme', initialTheme);
-      localStorage.setItem('color-theme', initialTheme);
+      setTheme(currentTheme);
+      document.documentElement.setAttribute('color-theme', currentTheme);
+      localStorage.setItem('color-theme', currentTheme);
     }
   }, []);
 
   // 테마 전환 함수
-  const toggleTheme = React.useCallback(() => {
+  const toggleTheme = useCallback(() => {
     setTheme(prevTheme => {
       const newTheme = prevTheme === 'light' ? 'dark' : 'light';
       if (typeof window !== 'undefined') {
